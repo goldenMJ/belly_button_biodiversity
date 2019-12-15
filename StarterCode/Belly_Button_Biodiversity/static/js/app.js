58eeb0475c41002f17948a22119c61fb5b132b65
goldenMJ <kadar.s.amek@gmail.com>
2019-12-14 20:12:22 -0800

    complete project

--- StarterCode/Belly_Button_Biodiversity/static/js/app.js
+++ StarterCode/Belly_Button_Biodiversity/static/js/app.js
@@ -1,30 +1,83 @@
 function buildMetadata(sample) {
-
-  // @TODO: Complete the following function that builds the metadata panel
-
   // Use `d3.json` to fetch the metadata for a sample
-    // Use d3 to select the panel with id of `#sample-metadata`
-
-    // Use `.html("") to clear any existing metadata
-
-    // Use `Object.entries` to add each key and value pair to the panel
-    // Hint: Inside the loop, you will need to use d3 to append new
-    // tags for each key-value in the metadata.
-
-    // BONUS: Build the Gauge Chart
-    // buildGauge(data.WFREQ);
+  var metadata = d3.select("#sample-metadata");
+  var url = "/metadata/" + sample;
+  // document.getElementById("#sample-metadata").innerHTML = "";
+  d3.json(url).then(function(response) {
+    console.log(response);
+    $("#sample-metadata").empty();
+    Object.entries(response).forEach(([key, value]) => metadata.append("p").text(`${key}: ${value}`));
+  });
 }
 
 function buildCharts(sample) {
+  var url = "/samples/" + sample;
+  d3.json(url).then(function(response) {
+    console.log(response);
+    var filtered_values = [];
+    var filtered_otu_ids = [];
+    var filtered_otu_labels = [];
+    var len = response.sample_values.length;
+    var indices = new Array(len);
+    for (var i = 0; i < len; i++) {
+      indices[i] = i;
+      indices.sort(function (a, b) { return response.sample_values[a] < response.sample_values[b] ? 1 : response.sample_values[a] > response.sample_values[b] ? -1 : 0; });
+    }    
+    for (var i =0; i<10; i++){
+      var j = indices[i];
+      filtered_values.push(response.sample_values[j]);
+      filtered_otu_ids.push(response.otu_ids[j]);
+      filtered_otu_labels.push(response.otu_labels[j]);
+    }
 
-  // @TODO: Use `d3.json` to fetch the sample data for the plots
+    var layout1 = {
+      annotations: [
+        {
+          font: {
+            size: 15
+          },
+          showarrow: false,
+          text: "Top 10 Samples",
+          x: 0.3,
+          y: 0.5
+        }
+      ],
+      height: 500,
+      width: 500
+    };
+    var trace1 = [{
+      type: "pie",
+      values: filtered_values,
+      labels: filtered_otu_ids.map(String),
+      text: filtered_otu_labels,
+      hole: .4,
+      textinfo: 'percent'
+    }];
+    console.log(trace1);
+    var PIE = document.getElementById('pie');
+    Plotly.newPlot(PIE, trace1, layout1);
 
-    // @TODO: Build a Bubble Chart using the sample data
-
-    // @TODO: Build a Pie Chart
-    // HINT: You will need to use slice() to grab the top 10 sample_values,
-    // otu_ids, and labels (10 each).
+    var trace2 = [{
+      x: response.otu_ids,
+      y: response.sample_values,
+      text: response.otu_labels,
+      mode: 'markers',
+      marker: {
+        color:response.otu_ids,
+        size: response.sample_values
+      }
+    }];
+    var layout2 = {
+      title: 'Bubble chart for each sample',
+      showlegend: false,
+      height: 600,
+      width: 1400
+    };
+    console.log(trace2);
+    Plotly.newPlot('bubble', trace2, layout2);
+  });
 }
+
 
 function init() {
   // Grab a reference to the dropdown select element
@@ -51,6 +104,12 @@ function optionChanged(newSample) {
   buildCharts(newSample);
   buildMetadata(newSample);
 }
+
+$('select').on('change', function() {
+  var Sample = d3.select("#selDataset").property('value');
+  console.log( Sample );
+  optionChanged(Sample);
+});
 
 // Initialize the dashboard
 init();
